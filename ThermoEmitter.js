25235c324b550343a87412d2c826532f9a41ef0c
Pierre-Julien Cazaux <cazaux.pierre@gmail.com>
2016-12-30 20:49:17 +0100

    First stable version

--- ThermoEmitter.js
+++ ThermoEmitter.js
@@ -9,15 +9,15 @@ var rfSniffer = rpi433.sniffer({
 // Receive (data is like {code: xxx, pulseLength: xxx})
 rfSniffer.on('data', function (data) {
   if(data.code == decimalValue) {
-  	//console.log("Nope");
-  	return;
+    //console.log("Nope");
+    return;
   }
   var rb = decToBinary(data.code, 24);
   var id = rb.substring(0, 8);
   //console.log("Ack", data.code, rb, id)
   if(id == myObj.id) {
-  	console.log("Acknowledge received");
-  	process.exit();
+    console.log("Acknowledge received");
+    process.exit();
   }
   else console.log("Possible id: " + id + 'Code received: '+data.code+' pulse length : '+data.pulseLength);
 
@@ -38,25 +38,30 @@ var aValue = parseInt(process.argv[3] || 0);
 
 
 var myObj = {
-	id: 		decToBinary(1, 8), 		// 0 - 255 	(8 bits)
-	free: 		decToBinary(0, 5),		// free		(5 bits)
-	command: 	decToBinary(aCommand, 4),// 0 - 15 	(4 bits)
-	value: 		decToBinary(aValue, 7)	// 0 - 127 	(7 bits)
+  id:     decToBinary(1, 8),    // 0 - 255  (8 bits)
+  free:     decToBinary(0, 5),    // free   (5 bits)
+  command:  decToBinary(aCommand, 4),// 0 - 15  (4 bits)
+  value:    decToBinary(aValue, 7)  // 0 - 127  (7 bits)
 };
 
 var binaryValue = myObj.id + myObj.free + myObj.command + myObj.value;
 
 var decimalValue = parseInt(binaryValue, 2);
 
-console.log(binaryValue, decimalValue);
+//console.log(binaryValue, decimalValue);
 
-rfEmitter.sendCode(decimalValue, function(error, stdout) {
-  	if(!error) console.log("Sent: " + stdout);
-});
+var sendmessage = function() {
+  rfEmitter.sendCode(decimalValue, function(error, stdout) {
+      if(!error) console.log("Sent: " + stdout);
+  });
+  setTimeout(sendmessage, 2000);
+}
+
+sendmessage();
 
 function decToBinary (dec, bits) {
-	var binary = (dec).toString(2);
-	if (bits !== undefined && binary.length > bits) throw new Error("Value exeeded allowed bits.");
-	else while (binary.length < bits) binary = "0"+ binary;
-	return binary
+  var binary = (dec).toString(2);
+  if (bits !== undefined && binary.length > bits) throw new Error("Value exeeded allowed bits.");
+  else while (binary.length < bits) binary = "0"+ binary;
+  return binary
 }
